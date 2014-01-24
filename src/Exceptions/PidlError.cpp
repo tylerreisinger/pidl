969d565e3c432fb9aad3306fc15ba82eb4828b2d
Tyler Reisinger <reisinger.tyler@gmail.com>
2014-01-24 18:28:37 -0500

    Added base exception class

---
+++ src/Exceptions/PidlError.cpp
@@ -0,0 +1,45 @@
+/*
+ * PidlError.cpp
+ *
+ *  Created on: Jan 24, 2014
+ *      Author: tyler
+ */
+
+#include "PidlError.h"
+
+namespace pidl
+{
+
+PidlError::PidlError(std::string message):
+	m_message(message)
+{
+}
+
+PidlError::PidlError(const PidlError& other):
+	m_message(other.m_message)
+{
+}
+
+PidlError::PidlError(PidlError&& other) noexcept:
+	m_message(std::move(other.m_message))
+{
+}
+
+PidlError& PidlError::operator =(const PidlError& other)
+{
+	m_message = other.m_message;
+	return *this;
+}
+
+PidlError& PidlError::operator =(PidlError&& other) noexcept
+{
+	m_message = std::move(other.m_message);
+	return *this;
+}
+
+void PidlError::setMessage(const std::string& message)
+{
+	m_message = message;
+}
+
+}
---
+++ src/Exceptions/PidlError.h
@@ -0,0 +1,33 @@
+#ifndef PIDLERROR_H_
+#define PIDLERROR_H_
+
+#include <exception>
+#include <string>
+
+namespace pidl
+{
+///Base exception class for all exceptions in the pidl library
+class PidlError: public std::exception
+{
+public:
+	PidlError() = default;
+	PidlError(std::string message);
+	virtual ~PidlError() noexcept = default;
+
+	PidlError(const PidlError& other);
+	PidlError(PidlError&& other) noexcept;
+	PidlError& operator =(const PidlError& other);
+	PidlError& operator =(PidlError&& other) noexcept;
+
+	virtual const char* what() const noexcept {return m_message.c_str();}
+
+protected:
+
+	void setMessage(const std::string& message);
+
+	std::string m_message;
+};
+
+}
+
+#endif
